10e029121110eb0e1a99e99cfacf8fa50c76ab61
Elina <elinagirot1643@gmail.com>
2025-03-05 15:10:08 +0100

    test remove pizza

--- TestCartePizzeria.py
+++ TestCartePizzeria.py
@@ -3,7 +3,7 @@ from unittest.mock import  Mock
 from CartePizzeria import CartePizzeria
 
 def test_is_empty():
-    cartePizzeria = Mock()
+    cartePizzeria = CartePizzeria()
     assert cartePizzeria.is_empty() == True
 
 def test_add_pizza():
@@ -17,3 +17,14 @@ def test_add_pizza():
 
     assert carte.nb_pizzas() == 1
     assert mock_pizza in carte.pizzas
+
+def test_remove_pizza():
+    carte = CartePizzeria()
+    mock_pizza = Mock()
+    mock_pizza.name = "Bolognaise"
+    carte.add_pizza(mock_pizza)
+    carte.remove_pizza("Bolognaise")
+
+    assert carte.nb_pizzas() == 0
+
+
