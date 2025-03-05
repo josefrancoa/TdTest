e33a91fe96bd157de8bde7804c610be08708c44b
Elina <elinagirot1643@gmail.com>
2025-03-05 13:51:19 +0100

    modif pizza

--- Pizza.py
+++ Pizza.py
@@ -1,8 +1,13 @@
 class Pizza:
-    def __init__(self,name,ingredients,prix):
+    def __init__(self,name,ingredients,prix,description,base):
         self.__name = name
         self.__ingredients = ingredients
         self.__prix = prix
+        self.__description = description
+        if(base=="tomate" or base=="crème"):
+            self.__base = base
+        else:
+            raise ValueError("Le nom de la base n'est pas bon")
 
     @property
     def name(self):
@@ -28,4 +33,24 @@ class Pizza:
     def prix(self,p):
         self.__prix = p
 
+    @property
+    def description(self):
+        return self.__description
+    
+    @description.setter
+    def description(self,d):
+        self.__description = d
 
+    @property
+    def base(self):
+        return self.__base
+    
+    @base.setter
+    def base(self,b):
+        if(b=="tomate"or b=="crème"):
+            self.__base = b
+        else:
+            print("La base n'est pas valide")
+
+pizza = Pizza("name","ingr",10,"des","tomate")
+print(pizza.base)
