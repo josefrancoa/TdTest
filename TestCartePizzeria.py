f8ca3d8f41886c2fda0f32090eb996dd92e3bed4
José <80324714+josefrancoa@users.noreply.github.com>
2025-03-05 14:18:22 +0100

    ajout test add_pizza

---
+++ TestCartePizzeria.py
@@ -0,0 +1,15 @@
+import pytest
+from unittest.mock import  Mock
+from CartePizzeria import CartePizzeria
+
+def test_add_pizza():
+
+    carte = CartePizzeria()
+
+    mock_pizza = Mock()
+    mock_pizza.name = "Margherita"
+
+    carte.add_pizza(mock_pizza)
+
+    assert carte.nb_pizzas() == 1
+    assert mock_pizza in carte.pizzas
