82cf0aded27d48ab26497e0d40d3b7d082569cf4
Elina <elinagirot1643@gmail.com>
2025-03-07 21:52:45 +0100

    ajout tests

--- test_CartePizzeria.py
+++ test_CartePizzeria.py
@@ -1,46 +1,58 @@
 import pytest
 from unittest.mock import  Mock
 from CartePizzeria import CartePizzeria
+from Pizza import Pizza
+from Boisson import Boisson
+from Dessert import Dessert
 
 def test_is_empty():
     cartePizzeria = CartePizzeria()
     assert cartePizzeria.is_empty() == True
 
-'''def test_add_pizza():
+    pizza = Mock(spec=Pizza)
+    pizza.name = "Bolo"
+    cartePizzeria.add(pizza)
+    assert cartePizzeria.is_empty() == False
 
+    cartePizzeria.remove("Bolo")
+    assert cartePizzeria.is_empty() == True
+
+
+def test_add_pizza():
     carte = CartePizzeria()
 
-    mock_pizza = Mock()
+    mock_pizza = Mock(spec=Pizza)
     mock_pizza.name = "Margherita"
 
-    carte.add_pizza(mock_pizza)
+    carte.add(mock_pizza)
 
     assert carte.nb_pizzas() == 1
-    assert mock_pizza in carte.pizzas'''
+    assert mock_pizza in carte.pizzas
 
 
-'''def test_remove_pizza():
+
+def test_remove():
     carte = CartePizzeria()
-    mock_pizza = Mock()
+    mock_pizza = Mock(spec=Pizza)
     mock_pizza.name = "Bolognaise"
-    carte.add_pizza(mock_pizza)
-    carte.remove_pizza("Bolognaise")
+    carte.add(mock_pizza)
+    carte.remove("Bolognaise")
 
-    assert carte.nb_pizzas() == 0'''
+    assert carte.nb_pizzas() == 0
 
     
 def test_nb_pizza():
     carte = CartePizzeria()
     assert carte.nb_pizzas() == 0
     
-    mock_pizza1 = Mock()
+    mock_pizza1 = Mock(spec=Pizza)
     mock_pizza1.name = "Margherita"
-    carte.add_pizza(mock_pizza1)
+    carte.add(mock_pizza1)
     assert carte.nb_pizzas() == 1
     
-    mock_pizza2 = Mock()
+    mock_pizza2 = Mock(spec=Pizza)
     mock_pizza2.name = "Peperoni"
-    carte.add_pizza(mock_pizza2)
+    carte.add(mock_pizza2)
     assert carte.nb_pizzas() == 2
 
 
@@ -48,21 +60,22 @@ def test_nb_drinks():
     carte = CartePizzeria()
     assert carte.nb_drinks() == 0
 
-    drink1 = Mock()
+    drink1 = Mock(spec=Boisson)
     drink1.name = "Coca"
     carte.add(drink1)
     assert carte.nb_drinks() == 1
 
-    drink2 = Mock()
+    drink2 = Mock(spec=Boisson)
     drink2.name = "Ice Tea"
     carte.add(drink2)
     assert carte.nb_drinks() == 2
 
-def test_add_desserts():
+
+def test_nb_desserts():
     carte = CartePizzeria()
-    mock_dessert1 = Mock()
+    mock_dessert1 = Mock(spec=Dessert)
     mock_dessert1.name = "Tiramisu"
-    mock_dessert2 = Mock()
+    mock_dessert2 = Mock(spec=Dessert)
     mock_dessert2.name = "Flan"
 
     carte.add(mock_dessert1)
